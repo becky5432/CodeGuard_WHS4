fe3c6e124248df5b78a153f69c3ab3f09ab2a643
ely1570 <ely1570@kookmin.ac.kr>
2026-09-13 22:03:26 +0900

    fix: remove duplicated execution resource limits

--- runner/pipeline/execution.py
+++ runner/pipeline/execution.py
@@ -127,11 +127,6 @@ def create_execution_container(
         "security_opt": [
             f"no-new-privileges={str(EXECUTION_NO_NEW_PRIVILEGES).lower()}"
         ],
-      
-        "mem_limit": memory_limit_bytes,
-        "memswap_limit": memory_limit_bytes,
-        "nano_cpus": nano_cpus_limit,
-        "pids_limit": pids_limit,
         "mem_limit": memory_limit_bytes,
         "memswap_limit": memory_limit_bytes,
         "nano_cpus": nano_cpus_limit,
